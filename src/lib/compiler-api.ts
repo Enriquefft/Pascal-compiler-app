9bf6674dd5bfcc43847455c7d60d01880ca9a1fb
enrique flores <enriquefft2001@gmail.com>
2025-07-01 00:21:32 -0500

    Handle new compiler API errors

--- src/lib/compiler-api.ts
+++ src/lib/compiler-api.ts
@@ -1,10 +1,13 @@
 import { type AST } from "./ast";
 
 export interface CompilationResult {
-        tokens: { token_name: string; token_content: string }[];
-        ast: AST;
-        asm: string;
-        output: string;
+        tokens?: { token_name: string; token_content: string }[];
+        ast?: AST;
+        asm?: string;
+        output?: string;
+        error?: string;
+        line?: number;
+        column?: number;
 }
 
 export async function compilePascal(code: string): Promise<CompilationResult> {
